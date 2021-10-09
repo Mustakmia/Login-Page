69f5b267099727693eecc6a15478102aa35e1383
Mustakmia <musyafijur52@gmail.com>
2021-10-09 22:27:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobeAsia } from '@fortawesome/free-solid-svg-icons'
 
+// variable for font awesome icon 
+const element = <FontAwesomeIcon icon={faGlobeAsia} />
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <div className="imgBx">
+          <img src="https://image.freepik.com/free-vector/halftone-background-with-circles_23-2148907689.jpg" alt="" />
+        </div>
+        <div className="contentBx">
+          <div className="formBx">
+            <h2>Login</h2>
+            <form>
+              <div className="inputBx">
+                <span>Username</span>
+                <input type="text" />
+              </div>
+              <div className="inputBx">
+                <span>Email</span>
+                <input type="text" />
+              </div>
+              <div className="inputBx">
+                <span>Password</span>
+                <input type="password" />
+              </div>
+              <div className="remember">
+                <label>
+                  <input type="checkbox" name="" />Remember
+                </label>
+              </div>
+              <div className="inputBx">
+                <input className="sign" type="Submit" value="Sign in" name="" />
+              </div>
+              <div className="inputBx">
+                <p>Don't have an account?  <a href="sign up">   Sign up</a></p>
+                <h3>Login social media</h3>
+                <ul className="sci">
+                  <li>{element}</li>
+                  <li>{element}</li>
+                  <li>{element}</li>
+                  <li>{element}</li>
+                </ul>
+              </div>
+            </form>
+            <div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <h3>llllllllllllllll</h3>
     </div>
+
   );
 }
 
---
+++ src/Firebase/firebase.confif.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBshU0EsHZ2HQUJPASRdz2UwebKRi7QOgA",
+    authDomain: "login-page-d7877.firebaseapp.com",
+    projectId: "login-page-d7877",
+    storageBucket: "login-page-d7877.appspot.com",
+    messagingSenderId: "301502768511",
+    appId: "1:301502768511:web:f211bb31dc0f0adf4e264a",
+    measurementId: "G-4HH6CFQM5R"
+};
+export default firebaseConfig;
